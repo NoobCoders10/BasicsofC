658044dc17c6f58db6b91ef736db42dd53a2f4ca
Deepak M S <souparnikad40@gmail.com>
2021-04-05 17:32:51 +0530

    defines the main function . Only the int function

---
+++ basicSyntx.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// 'include' is the keyword used to call the default modules to our code
+// 'stdio.h' means it is the header file of standerd input and output. 
+
+int main(){
+	return 0;
+}
+
+/*
+ it is the main function we are written in c language. the word 'int' defines the the data type of our function ie Integer . we can write two type of main function in c
+	1. int main function(above example)-if we define a int function we wanted to write return keyword . (it is mandatory).
+*/
