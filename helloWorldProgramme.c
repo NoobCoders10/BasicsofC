cca250a330bf3a7380448c3fc86c7d2ba6d016c2
Deepak M S <souparnikad40@gmail.com>
2021-04-06 12:21:01 +0530

    hello world programme added

---
+++ helloWorldProgramme.c
@@ -0,0 +1,17 @@
+/* Today we going to write a hello world programme in c . 
+ */
+
+// At first we want to include the header file
+#include <stdio.h>
+
+// then we want to write our main fuction . here I am going to write a int main function you can also write a void main function insted of int
+
+int main(){
+	
+	printf("Hello world");
+	//'printf' is the output function in used to print anything in the output console . The syntx of printf is , at first you want to write the keyword printf after that open a open bracket and write anything you wanted to print in the console in a double cots. Don't forget to add a semi-colon ';' after the open bracket. 
+	return 0;
+	// here we did'nt return any value from our main function. so we write as return 0;
+}
+
+// output of this programme will be Hello world.
