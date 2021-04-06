0fa79706a40f44ac4cf7c05f382216f1c679d446
Deepak M S <souparnikad40@gmail.com>
2021-04-06 12:06:57 +0530

    file finished

--- basicSyntx.c
+++ basicSyntx.c
@@ -1,3 +1,6 @@
+/* This file is for understanding the basic structure of c programme.
+ */
+
 #include<stdio.h>
 // 'include' is the keyword used to call the default modules to our code
 // 'stdio.h' means it is the header file of standerd input and output. 
@@ -9,4 +12,13 @@ int main(){
 /*
  it is the main function we are written in c language. the word 'int' defines the the data type of our function ie Integer . we can write two type of main function in c
 	1. int main function(above example)-if we define a int function we wanted to write return keyword . (it is mandatory).
+	2. also we can decleare a void function in c . such as 
+		#include <stdio.h>
+
+		void main(){
+		}
+	here it is not mandatory to write return keyword in void function
+
+	it is mandatory to write main function in every c programme.
+
 */
