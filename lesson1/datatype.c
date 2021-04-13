9c80d8432e7c02eed3160a9dbe5ac4936f325927
Deepak M S <souparnikad40@gmail.com>
2021-04-13 22:47:37 +0530

    datatype.c file moved to the lesson 2 folder and name of that file modified

--- lesson1/datatype.c
+++
@@ -1,9 +0,0 @@
-/*this file help to print data size of datatypes
-*/
-#include<stdio.h>
-int main(){
-
-    //to print datasize if integer
-
-    return 0;
-}
---
+++ lesson2/2.1.2datatype.c
@@ -0,0 +1,11 @@
+/*this file help to print data size of datatypes
+*/
+#include<stdio.h>
+int main(){
+
+    //to print datasize of any datatype just use a  fuction called sizeof. for example
+    //in case of integer
+    printf("%d",sizeof(int));
+
+    return 0;
+}
